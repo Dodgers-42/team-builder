2b334e966ca86d4ec0c7f6088dd6a1a31c77bdc9
Dodger-42 <kevin.bing.barber@gmail.com>
2020-07-09 19:51:59 -0700

    first

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
+  
+  const [teamMembersList, setTeamMembersList] = useState({});
+
+
   return (
     <div className="App">
       <header className="App-header">
